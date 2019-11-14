42577a9de1b9c8cafbc4f119bebbaafa653f9c57
jesusromeroguti <jrg.jesusromero@gmail.com>
2019-11-14 11:39:55 +0100

    Agefida implementacio constructor per copia, operador assignacio, funcio numero, nom i frequencia i operadors posincrement i preincrement

--- phone.cpp
+++ phone.cpp
@@ -2,11 +2,32 @@
 
 phone::phone(nat num, const string& name, nat compt) throw(error){
   _num = num;
+  for(int i = 0; i < name.size(); i++){
+    if(name[i] == DELETECHAR || name[i] == ENDCHAR || name[i] == ENDPREF){
+      throw error(ErrNomIncorrecte);
+    }
+  }
   _name = name;
   _compt = compt;
 }
 
+phone::phone(const phone& T) throw(error){
+  _num = T._num;
+  _name = T._name;
+  _compt = T._compt;
+}
+
+phone& phone::operator=(const phone& T) throw(error){
+  if(this != &T){
+    _num = T._num;
+    _name = T._name;
+    _compt = T._compt;
+  }
+  return *this;
+}
+
 phone::~phone() throw() {}
+
 
 nat phone::numero() const throw(){
   return _num;
@@ -19,4 +40,35 @@ string phone::nom() const throw(){
 
 nat phone::frequencia() const throw() {
   return _compt;
+}
+
+phone& phone::operator++() throw(){
+  _compt++;
+  return *this;
+}
+
+phone phone::operator++(int) throw(){
+  phone aux(*this);
+  _compt++;
+  return aux;
+}
+
+bool phone::operator>(const phone& T) const throw(){
+  // bool t;
+  // if(_compt > T._compt) return true;
+  // else if(_compt == T._compt){
+  //   if(){}
+  // }
+
+}
+
+bool phone::operator==(const phone& T) const throw(){
+  bool t = true;
+  if(_compt != T._compt) return false;
+  else {
+    for(int i = 0; i < _name.size(); i++){
+      if(_name[i] == T._name[i]) return false;
+    }
+  }
+  return t;
 }
--- proba.cpp
+++ proba.cpp
@@ -4,9 +4,16 @@
 using namespace std;
 
 int main(){
-  phone p(123,"Jesus",1);
+  phone p(123,"Jesus",2);
   cout << p.nom() << endl;
   cout << p.numero()<< endl;
   cout << p.frequencia() << endl;
-  return 0;
+
+  phone l(123,"Jesus",2);
+  cout << l.nom() << endl;
+  cout << l.numero()<< endl;
+  cout << l.frequencia() << endl;
+
+  bool b = p == l;
+  cout << b << endl;
 }
